8271d3957bb95bc11097467b7549f54bc4d2a7f5
Katy Henning <katyannhenning@gmail.com>
2016-08-18 18:59:37 -0700

    add JavaScript that lists all the numbers of user input from the input number to one

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,27 @@
-// backend
-
+//backend
+function myFunction (userinput) {
+  for (var i = 0; i <= userInput; index =+1)
+    alert(userInput);
+}
 
 
 
 //front end user interface
 $(document).ready(function(){
-  $("button").submit(function(event){
+  $("button").click(function(event){
     event.preventDefault();
-    var userInput = $("#number").val();
-    $("ul").append("<li>" + userInput + "</li>");
+    var userInput = parseInt($("#number").val());
+    for (var index = 1; index <= userInput; index += 1) {
+      if (userInput >= 2) {
+        userInput -= 1;
+        $("#results").append("<li>" + userInput + "</li>");
+      }
+        else  {
+        alert("enter a number larger than 2")
+      }
+
+  }
+
 
   });
 });
