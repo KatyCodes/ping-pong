72233fc3cfcda4338c18f6fd1ca647262e194def
Katy Henning <katyannhenning@gmail.com>
2016-08-19 09:08:07 -0700

    add script so that numbers divisible by 5 and 15 are replaced by pong and ping-pong

--- js/scripts.js
+++ js/scripts.js
@@ -11,15 +11,21 @@ $(document).ready(function(){
         alert("please enter a number larger than 2");
       }
        else {
-        for (var index = 1; index=userInput; index =+1) {
+        for (var index = ""; index=userInput; index =+1) {
           userInput -= 1;
-        $("#results").append("<li>" + userInput + "</li>");
-          if ((userInput - 1) % 3=== 0) {
-            $("#results").append("<li>" + "ping" + "</li>");
+        $("#results").prepend("<li>" + userInput + "</li>");
+       if ((userInput - 1) % 15=== 0) {
+            $("#results").prepend("<li>" + "ping-pong" + "</li>");
             userInput -=1
-            // {"continue";}
           }
-      }
+          if ((userInput - 1) % 5=== 0) {
+               $("#results").prepend("<li>" + "pong" + "</li>");
+               userInput -=1
+          } else if ((userInput - 1) % 3=== 0) {
+           $("#results").prepend("<li>" + "ping" + "</li>");
+           userInput -=1
+         }
         }
+      }
   });
 });
